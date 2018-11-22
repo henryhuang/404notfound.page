01cad358bcf19a136c9461b2ff6f407154129d88
henryhuang <h1886@outlook.com>
2018-11-22 23:45:52 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import 'bulma';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container is-fullhd">
+
+          <section className="hero">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  404 Not Found Page
+                </h1>
+                <h2 className="subtitle">
+                  Create useful 404 not found page for your website.
+                </h2>
+              </div>
+            </div>
+          </section>
+
+        </div>
+    );
+  }
+}
+
+export default App;
